515377e98fea03613094e0abd9160e37a9482eef
shreyashbrn <shreyashbrn@gmail.com>
2025-08-10 17:12:46 +0530

    testing git

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send("Backend is running");
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on the PORT: http://localhost:{PORT}`);
+    
+})
