31d71b9602562863c1ed44abd398daa62882664c
shreyashbrn <shreyashbrn@gmail.com>
2025-10-11 04:28:22 +0530

    added refresh button in dashboard to fetch real time data

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,53 @@
 // src/pages/Dashboard.js
 import React from "react";
+import { FiRefreshCcw } from "react-icons/fi"; 
+import { useState } from "react";
 
 const Dashboard = () => {
+
+
+  const [rotate, setRotate] = useState(false);
+
+  const handleClick = () => {
+    setRotate(true);
+    setTimeout(() => setRotate(false), 1000); // stop rotation after 1 second
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
+
     <div className="text-black ml-12 flex flex-col gap-6">
       <div className="flex items-center gap-5">
 
       <div className="flex ">
         <div className="flex flex-col gap-4">
         <p className="text-[45px] text-white">11 October</p>
-          <div className="w-[420px] h-[70px] bg-[#12121a]  rounded-[40px] text-center">box1</div>
+          <div className="w-[420px] h-[70px] bg-[#12121a]  rounded-[40px] text-white flex justify-between items-center pl-6 pr-6">
+            <p className="w-[300px]">Use the refresh button anytime to see the most recent updates.</p>
+            <button
+        onClick={handleClick}
+        className="p-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition"
+      >
+        <FiRefreshCcw size={20}
+          className={`text-3xl ${rotate ? "animate-spin" : ""} transition-transform`}
+        />
+      </button>
+
+            </div>
           <div className="w-[420px] h-[170px] bg-[#12121a]  rounded-[20px]">box2</div>
         </div>
       </div>
