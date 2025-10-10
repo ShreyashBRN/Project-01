218a75af2d9654ac0bd050434f5f76cb8dbed40a
shreyashbrn <shreyashbrn@gmail.com>
2025-10-11 01:51:15 +0530

    Sidebar functionality completed

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,21 +2,34 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { GoHome } from "react-icons/go";
+import { RiAdminLine } from "react-icons/ri";
+import { MdDashboard } from "react-icons/md";
+import { TbReportAnalytics } from "react-icons/tb";
+import { MdInventory } from "react-icons/md";
+import { GrUserWorker } from "react-icons/gr";
+import { CiExport } from "react-icons/ci";
+
 
 const Sidebar = () => {
   return (
     <div className="w-64 h-screen bg-[#12121a] text-white flex flex-col p-4">
-      <h2 className="text-2xl font-bold mb-6">Admin Panel</h2>
+      <div className="flex items-center gap-2">
+        <RiAdminLine  className="w-8 h-8"/>
+      <h2 className="text-2xl font-bold ">Admin Panel</h2>
+      </div>
 
       <NavLink
         to="/dashboard"
         end
         className={({ isActive }) =>
-          `p-2 rounded mb-2 hover:bg-slate-600 ${isActive ? "bg-slate-700" : ""}`
+          `p-2 mt-5 rounded mb-2 hover:bg-slate-600 ${isActive ? "bg-slate-700" : ""}`
         }
       >
-        
+        <div className="flex items-center gap-3">
+          <MdDashboard />
         Dashboard
+        </div>
+        
       </NavLink>
       <NavLink
         to="/dashboard/analytics"
@@ -24,7 +37,11 @@ const Sidebar = () => {
           `p-2 rounded mb-2 hover:bg-slate-600 ${isActive ? "bg-slate-700" : ""}`
         }
       >
+        <div className="flex items-center gap-3">
+          <TbReportAnalytics />
         Analytics
+        </div>
+        
       </NavLink>
 
       <NavLink
@@ -33,7 +50,11 @@ const Sidebar = () => {
           `p-2 rounded mb-2 hover:bg-slate-600 ${isActive ? "bg-slate-700" : ""}`
         }
       >
+        <div className="flex  items-center gap-3">
+          <MdInventory />
         Inventory
+        </div>
+        
       </NavLink>
 
       <NavLink
@@ -42,7 +63,11 @@ const Sidebar = () => {
           `p-2 rounded mb-2 hover:bg-slate-600 ${isActive ? "bg-slate-700" : ""}`
         }
       >
+        <div className="flex items-center gap-3">
+          <GrUserWorker />
         Employees
+        </div>
+        
       </NavLink>
 
       <NavLink
@@ -51,7 +76,11 @@ const Sidebar = () => {
           `p-2 rounded mb-2 hover:bg-slate-600 ${isActive ? "bg-slate-700" : ""}`
         }
       >
+        <div className="flex items-center gap-3">
+          <CiExport />
         Export
+        </div>
+        
       </NavLink>
     </div>
   );
