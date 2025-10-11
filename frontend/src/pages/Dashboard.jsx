fbd7be0e3730d2fdb5d87163feca3be04c5574f8
shreyashbrn <shreyashbrn@gmail.com>
2025-10-11 18:07:07 +0530

    added dependencies of chakra UI - progress bar, progress circle

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,6 +2,9 @@
 import React from "react";
 import { FiRefreshCcw } from "react-icons/fi"; 
 import { useState } from "react";
+import { HiMiniCurrencyDollar } from "react-icons/hi2";
+
+
 
 const Dashboard = () => {
 
@@ -12,7 +15,11 @@ const Dashboard = () => {
     setRotate(true);
     setTimeout(() => setRotate(false), 1000); // stop rotation after 1 second
   };
+ const [progress, setProgress] = useState(50);
 
+  const handleChange = (e) => {
+    setProgress(Number(e.target.value));
+  };
 
 
 
@@ -30,14 +37,14 @@ const Dashboard = () => {
 
   return (
 
-    <div className="text-black ml-12 flex flex-col gap-6">
+    <div className="text-white ml-12 flex flex-col gap-6">
       <div className="flex items-center gap-5">
 
       <div className="flex ">
         <div className="flex flex-col gap-4">
         <p className="text-[45px] text-white">11 October</p>
           <div className="w-[420px] h-[70px] bg-[#12121a]  rounded-[40px] text-white flex justify-between items-center pl-6 pr-6">
-            <p className="w-[300px]">Use the refresh button anytime to see the most recent updates.</p>
+            <p className="w-[300px] text-[13px] pt-1 pb-1 leading-[14px]">Use the refresh button to fetch the most recent data in real time and latest system activity.</p>
             <button
         onClick={handleClick}
         className="p-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition"
@@ -48,12 +55,33 @@ const Dashboard = () => {
       </button>
 
             </div>
-          <div className="w-[420px] h-[170px] bg-[#12121a]  rounded-[20px]">box2</div>
+          <div className="w-[420px] h-[170px] bg-[#12121a] flex rounded-[20px] items-center text-center gap-5">
+            <div>
+            <HiMiniCurrencyDollar size={110} color="white" />
+            </div>
+            <div>
+              <p className="text-[20px] mb-1">Total Revenue</p>
+              <p className="text-[35px] font-bold">$45,970</p>
+              <input
+        type="range"
+        min="0"
+        max="100"
+        value={progress}
+        onChange={handleChange}
+        className="w-[230px] accent-[#3b82f6] mt-3 cursor-pointer"
+      />
+
+
+            </div>
+
+          </div>
         </div>
       </div>
 
         <div className="h-[250px] w-[300px]  bg-[#12121a]  mt-[89px] rounded-[20px]">
-          box3
+          <p></p>
+
+          <p></p>
           </div>
 
       <div className="flex flex-col gap-3">
